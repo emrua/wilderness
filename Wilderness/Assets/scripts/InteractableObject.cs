272c4a8d697585b8a2fc0cb62916f83253e3a6fc
emrua <emmaanglin87@gmail.com>
2024-11-05 18:14:09 -0500

    terrain update

--- Wilderness/Assets/scripts/InteractableObject.cs
+++ Wilderness/Assets/scripts/InteractableObject.cs
@@ -6,9 +6,46 @@ using TMPro;
 public class InteractableObject : MonoBehaviour
 {
     public string ItemName;
+    public bool PlayerInRange;
  
     public string GetItemName()
     {
         return ItemName;
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && PlayerInRange && SelectionManager.instance.onTarget)
+        {
+            if(!InventorySystem.Instance.CheckIfFull())
+            {
+            InventorySystem.Instance.AddToInventory(ItemName);
+            Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Invenotry is full");
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerInRange = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if(other.CompareTag("Player"))
+        {
+            PlayerInRange = false;
+        }
     }
 }
--- Wilderness/Assets/scripts/SelctionManager.cs
+++ Wilderness/Assets/scripts/SelctionManager.cs
@@ -8,14 +8,30 @@ using TMPro;
 public class SelectionManager : MonoBehaviour
 {
  
+    public static SelectionManager instance {get; set;}
+
+    public bool onTarget;
     public GameObject interaction_Info_UI;
     TMP_Text interaction_text;
  
     private void Start()
     {
+        onTarget = false;
         interaction_text = interaction_Info_UI.GetComponent<TMP_Text>();
     }
  
+    public void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+          instance = this;  
+        }
+    }
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,17 +39,29 @@ public class SelectionManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
- 
-            if (selectionTransform.GetComponent<InteractableObject>())
-            {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+
+            if (interactable && interactable.PlayerInRange)
+            {   
+                onTarget = true;
+
+                interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else 
             { 
+                onTarget = false;
                 interaction_Info_UI.SetActive(false);
             }
+        }
+            else 
+            { 
+                onTarget = false;
+                interaction_Info_UI.SetActive(false);
+            }
+    
  
         }
     }
-}
+
